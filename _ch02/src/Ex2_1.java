1f2a65b5707d92b5e932d1385abddce216d93fcc
bborabora-ko <bborabora.ko@gmail.com>
2022-03-29 18:16:48 +0900

    ch2-1~2. print(), println()

---
+++ _ch02/src/Ex2_1.java
@@ -0,0 +1,15 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		System.out.print("5+3"); //5+3 print() - 출력 후 줄바꿈x
+		System.out.println("5+3"); //5+3 println() - 출력 후 줄바꿈o
+		System.out.println(5+3); //8
+		System.out.println(5-3); //2
+		System.out.println(5*3); //15
+		System.out.println(5/3); //1
+		System.out.println(5/3.0); //1.3333333
+		
+	}
+
+}
