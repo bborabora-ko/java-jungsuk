a2e9ad0d510f5c81007cf33c1391c85c9493debe
bborabora-ko <bborabora.ko@gmail.com>
2022-03-30 14:49:15 +0900

    ch2-3. variable & arithmetic operation

---
+++ ch02/src/VarEx1.java
@@ -0,0 +1,20 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		//º¯¼ö¸¦ »ç¿ëÇÏÁö ¾ÊÀº »çÄ¢¿¬»ê
+		System.out.println(4 + 2); //6 µ¡¼À
+		System.out.println(4 - 2); //2 »¬¼À
+		System.out.println(4 * 2); //8 °ö¼À
+		System.out.println(4 / 2); //2 ³ª´°¼À
+		
+		//º¯¼ö¸¦ »ç¿ëÇÑ »çÄ¢¿¬»ê
+		int x = 4, y = 2;
+		System.out.println(x + y); //6
+		System.out.println(x - y); //2
+		System.out.println(x * y); //8
+		System.out.println(x / y); //2
+		
+	}
+
+}
