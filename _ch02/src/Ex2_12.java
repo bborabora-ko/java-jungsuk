bc372875287168f85f09c197d278b5aa4322fb58
bborabora-ko <bborabora.ko@gmail.com>
2022-05-09 17:53:13 +0900

    .

---
+++ _ch02/src/Ex2_12.java
@@ -0,0 +1,21 @@
+
+public class Ex2_12 {
+
+	public static void main(String[] args) {
+		String str = "3"; //문자열3
+		
+		System.out.println(str.charAt(0) - '0'); //문자3 -> 숫자3
+		System.out.println('3' - '0' +1); //4
+		System.out.println(Integer.parseInt(str)+ 1); //4
+		System.out.println("3" + 1); //문자열 31
+		
+		System.out.println('3' - '0'); //숫자3
+		System.out.println(3 + '0'); //51
+		System.out.println((char)(3 + '0'));//문자3
+
+		
+		
+		
+	}
+
+}
