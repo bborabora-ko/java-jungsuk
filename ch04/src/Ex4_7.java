4e3d31b9fa82f3214c3e3d59dfb45a2b7f504a0f
bborabora-ko <bborabora.ko@gmail.com>
2022-05-10 16:43:00 +0900

    ch4-7. Math.random()

---
+++ ch04/src/Ex4_7.java
@@ -0,0 +1,16 @@
+
+public class Ex4_7 {
+	public static void main(String[] args) {
+		int num = 0;
+
+		// 1. 1~10 사이의 난수를 20개 출력하기
+		for (int i = 0; i <= 20; i++) {
+			System.out.println((int) (Math.random() * 10) + 1);
+		}
+
+		// 2. -5~5 사이의 난수를 20개 출력하시오.
+		for (int i = 0; i <= 20; i++) {
+			System.out.println((int) (Math.random() * 11) - 5); 
+		}
+	}
+}
