bb0a227d1469eb35c081aec681752fc9fdc35de4
bborabora-ko <bborabora.ko@gmail.com>
2022-06-08 08:17:43 +0900

    ch7-4. composition

---
+++ ch07/src/inheritanceTest.java
@@ -0,0 +1,38 @@
+import java.security.spec.X509EncodedKeySpec;
+
+class MyPoint{
+	int x;
+	int y;
+}
+
+//class Circle extends MyPoint{ //상속
+//	int r;
+//}
+
+class Circle { //포함
+	MyPoint m = new MyPoint();
+	int r;
+}
+
+public class inheritanceTest {
+
+	public static void main(String[] args) {
+//		Circle c = new Circle(); //상속ㄱ
+//		c.x = 1;
+//		c.y = 2;
+//		c.r = 3;
+//		System.out.println("c.x=" + c.x);
+//		System.out.println("c.y=" +c.y);
+//		System.out.println("c.r=" + c.r);
+		
+		Circle c = new Circle(); //포함
+		c.m.x = 1;
+		c.m.y = 2;
+		c.r = 3;
+		System.out.println("c.m.x=" + c.m.x);
+		System.out.println("c.m.y=" + c.m.y);
+		System.out.println("c.r=" +c.r);
+		
+	}
+
+}
